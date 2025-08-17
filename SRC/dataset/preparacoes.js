ffd2c1f23a6f8eb971f321e8f09b5c07ebe58449
janaylson-369 <janailson.gomes@academico.ifpb.edu.br>
2025-08-17 15:29:19 -0300

    Add files via upload

---
+++ SRC/dataset/preparacoes.js
@@ -0,0 +1,34 @@
+let preparacoesCard = [
+    {
+        src: "/assets/image/café preto.png",
+        nome: "café Preto",
+        descricao: "café preto pronto para o seu consumo",
+        preco: "R$ 10,00"
+    },
+    {
+        src: "/assets/image/cappucino.png",
+        nome: "Cappuccino",
+        descricao: "Cappucino dilatada com cafeina proteica",
+        preco: "R$ 25,00"
+    },
+    {
+        src: "/assets/image/Pacote de cafe.png",
+        nome: "Pacote de café",
+        descricao: "pó de café feito para todos os gostos",
+        preco: "R$ 100,00"
+    },
+    {
+        src: "/assets/image/Expresso.png",
+        nome: "Café Expresso",
+        descricao: "café expresso dilatada com cafeina proteica",
+        preco: "R$ 15,00"
+    }
+];
+
+
+
+let preparacoesCardapio = [];
+
+let inserirPreparacao = () => { };
+
+export { preparacoesCard, preparacoesCardapio, inserirPreparacao };
